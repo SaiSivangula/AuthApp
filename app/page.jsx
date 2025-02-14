4c38f9954d890762345f3830718dd4d86dfa06c0
sivangula sai <saisivangula242@gmail.com>
2025-02-14 13:34:39 +0530

    page.jsx updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,17 @@
 import LoginForm from "@/Components/LoginForm";
 import { getServerSession } from "next-auth";
-import { redirect } from 'next-auth';
+import { redirect } from 'next/navigation';
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
   const session  = await getServerSession(authOptions);
 
-  if(session) redirect("/dashboard");
-  return <main>
+  if(session) {
+    redirect('/dashboard');
+  }
+  return(
+  <main>
     <LoginForm/>
   </main>
+  );
 }
